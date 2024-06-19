e7932b5d5a906181406cdfdc55b339b8b45fa6d9
sea <125@qq.com>
2024-06-19 21:30:35 +0800

    定义了一个有关mysql操作的源文件和头文件

---
+++ server/include/mysqloperate.h
@@ -0,0 +1,9 @@
+#ifndef MYSQL_OPERATE_H
+#define MYSQL_OPERATE_H
+
+#include <mysql/field_types.h>
+#include <mysql/mysql.h>
+int getpwd(MYSQL* mysql, int uid);
+
+
+#endif;
---
+++ server/src/mysqloperate.c
@@ -0,0 +1,37 @@
+#include "../include/dbpool.h"
+#include "../include/head.h"
+//获取当前路径,传入连接和用户uid，传出当前目录的索引
+int getpwd(MYSQL* mysql, int uid){
+    const char sql[60] = "select pwd from nb_usertable where id = ?";
+    MYSQL_STMT* stmt = mysql_stmt_init(mysql);
+    mysql_stmt_prepare(stmt, sql, strlen(sql));
+    //初始化参数
+    MYSQL_BIND bind;
+    bzero(&bind, sizeof(bind));
+    bind.buffer_type = MYSQL_TYPE_LONG;
+    bind.buffer = &uid;
+    bind.length = NULL;
+    bind.is_null = 0;
+
+    mysql_stmt_bind_param(stmt, &bind);
+
+    mysql_stmt_execute(stmt);
+
+    MYSQL_RES* res = mysql_stmt_execute(stmt);
+    //获取结果
+    int res_pwd;
+    MYSQL_BIND res_bind;
+    bzero(&res_bind, sizeof(res_bind));
+    res_bind.buffer_type = MYSQL_TYPE_LONG;
+    res_bind.buffer = &res_pwd;
+    res_bind.buffer_length = sizeof(int);
+
+    mysql_stmt_bind_result(stmt, &bind);
+    mysql_stmt_store_result(stmt);
+    mysql_stmt_fetch(stmt);
+    
+    mysql_stmt_close(stmt);
+    mysql_free_result(res);
+
+    return res_pwd;
+}
