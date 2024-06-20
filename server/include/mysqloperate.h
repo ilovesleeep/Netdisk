c598fec81e2413b1aed5a1aed7811f8a54f6529e
huwentaocs <huwentaocs@foxmail.com>
2024-06-20 16:02:29 +0800

    保存本地修改

--- server/include/mysqloperate.h
+++ server/include/mysqloperate.h
@@ -7,4 +7,4 @@ int getPwdId(MYSQL* mysql, int uid);
 char* getPwd(MYSQL* mysql, int pwdid);
 int goToRelativeDir(MYSQL* mysql, int pwd, char* path);
 char** findchild(MYSQL*mysql, int pwdid);
-#endif;
+#endif
